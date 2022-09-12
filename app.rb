2e34ecabfc7f8fa99c0fecf5a13fc74e23d051ee
Yichao <yq2550@gmail.com>
2022-09-12 14:42:11 +0100

    exercise and challenge from 02 completed

--- app.rb
+++ app.rb
@@ -7,4 +7,25 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  get '/' do
+    return "hello!"
+  end
+
+  get '/hello' do
+    name = params[:name]
+    return "Hello #{name}"
+  end
+
+  get '/posts' do
+    name = params[:name]
+    cohort_name = params[:cohort_name]
+    return "Hello #{name}, you are in #{cohort_name}"
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+    return "Thanks #{name}, you sent this message: #{message}"
+  end
 end
