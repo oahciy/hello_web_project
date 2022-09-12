7277ac15aead431929c4e0b523d80058a83c447c
Yichao <yq2550@gmail.com>
2022-09-12 16:39:38 +0100

    exercise from 03 done
    Co-authored-by: MarinaIvanova-1<mariwkaivanova@gmail.com>

--- app.rb
+++ app.rb
@@ -12,20 +12,14 @@ class Application < Sinatra::Base
     return "hello!"
   end
 
-  get '/hello' do
-    name = params[:name]
-    return "Hello #{name}"
-  end
-
-  get '/posts' do
-    name = params[:name]
-    cohort_name = params[:cohort_name]
-    return "Hello #{name}, you are in #{cohort_name}"
-  end
-
   post '/submit' do
     name = params[:name]
-    message = params[:message]
-    return "Thanks #{name}, you sent this message: #{message}"
+    age = params[:age]
+    return "#{name}: #{age}"
+  end
+
+  get '/names' do
+    name = params[:name]
+    return "#{name}"
   end
 end
---
+++ spec/integration/app_spec.rb
@@ -0,0 +1,51 @@
+# file: spec/integration/application_spec.rb
+
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+  # generates a method called 'app' which returns a new instance of 'Application'
+
+
+  context "GET to /" do
+    it "returns 200 OK with the right content" do
+      # Send a GET request to /
+      # and returns a response object we can test.
+      response = get("/")
+
+      # Assert the response status code and body.
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("hello!")
+    end
+  end
+
+  context "POST to /submit" do
+    it "returns 200 OK with the right content" do
+      # Send a POST request to /submit
+      # with some body parameters
+      # and returns a response object we can test.
+      response = post("/submit", name: "Yichao", age: 24)
+
+      # Assert the response status code and body.
+      expect(response.status).to eq(200)
+      expect(response.body).to eq("Yichao: 24")
+    end
+  end
+
+  context "GET /names" do
+    it 'returns a string of names' do
+
+      response = get('/names?name=Julia, Mary, Karim')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('Julia, Mary, Karim')
+    end
+  end
+end
